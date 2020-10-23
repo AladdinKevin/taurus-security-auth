858b4be1f6a239b6a05be99206afccf79deac191
xukai <xukaixfs@xinfushe.com>
2020-10-23 10:18:08 +0800

    app browser demo first commit

---
+++ app/src/test/java/com/taurus/security/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taurus.security.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
