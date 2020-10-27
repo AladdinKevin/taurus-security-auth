2a36c1a22e324d36ed229d055d93ee7b18992fad
xukai <18813008034@163.com>
2020-10-27 23:29:17 +0800

    禁用SpringSecurity安全配置

--- demo/src/main/java/com/taurus/security/DemoApplication.java
+++ demo/src/main/java/com/taurus/security/DemoApplication.java
@@ -2,9 +2,14 @@ package com.taurus.security;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
+/**
+ * https://blog.csdn.net/yuanboqi/article/details/109238308
+ * 禁用SpringSecurity安全配置
+ */
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 @MapperScan("com.taurus.security.demo.dao")
 public class DemoApplication {
 
